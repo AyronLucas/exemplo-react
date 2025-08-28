8c4a668d228397eaa6ffe883c0c9f06cbb0ddca7
Ayron <ayronlucas1@gmail.com>
2025-08-27 21:57:26 -0300

    feat/add-react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './componets/pages/Home'
+import About from './componets/pages/About'
+import Header from './componets/Header'
+import Footer from './componets/Footer'
 
 
 
 function App() {
   return (
   <>
-    <Home />
+  <Header />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='About' element={<About />} />
+    </Routes>
+    <Footer/>
   </>
         )
 }
--- src/componets/Header/index.jsx
+++ src/componets/Header/index.jsx
@@ -1,9 +1,20 @@
+import { Link } from 'react-router-dom'
 import './style.css'
 
 function Header(){
     return (
         <header>
         <h1>Minha primeira página React</h1>
+        <nav>
+          <ul>
+            <Link to='/'>
+              <li>Inicio</li>
+            </Link>
+            <Link to='About'>
+              <li>Sobre</li>
+            </Link>
+          </ul>
+        </nav>
       </header>
     )
 }
